1ed8b82300434525a754901d8eed4efa416b6b73
Patrick Schaumont <pschaumont@wpi.edu>
2022-10-05 01:05:14 +0000

    upload

---
+++ rtl/div.v
@@ -0,0 +1,12 @@
+module div(input  wire [7:0] y,
+	   input wire [7:0]  x,
+	   output wire [7:0] r,
+	   output wire [7:0] q,
+	   input wire 	     start,
+	   output wire 	     ready,
+	   input wire 	     clk,
+	   input wire 	     reset);
+
+   // implement a restoring divider on 8-bit precision
+   
+endmodule
